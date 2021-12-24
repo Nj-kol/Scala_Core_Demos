b21b3ab8c369a39fcfaa9a4b9519912c46236171
Nj-Kol <nilanjan.sarkar100@gmail.com>
2021-12-24 17:16:48 +0530

    latest

--- scala-pattern-matching/src/main/scala/example/PatternMatchingDemo.scala
+++ scala-pattern-matching/src/main/scala/example/PatternMatchingDemo.scala
@@ -5,10 +5,4 @@ case class Paypal(email: String) extends Account
 case class Bitcoin(key: String) extends Account
 case class User(name: String,account: Account)
 
-def whatis(obj: Any) {
-	obj match {
-		case User(name,Paypal(email)) => s"Paypal with email : ${email}"
-		case User(name,Bitcoin(key))  => s"Bitcoin with key : ${Key}"
-		case _                        => "Unknown type"
-	}
-}
+
