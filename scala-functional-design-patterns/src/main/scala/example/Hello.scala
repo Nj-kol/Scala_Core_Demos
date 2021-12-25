4dde765f7846c4aec1e35a0dabab7d087ff1d01b
Nj-Kol <nilanjan.sarkar100@gmail.com>
2021-12-25 22:01:12 +0530

    Added cake pattern

--- scala-functional-design-patterns/src/main/scala/example/Hello.scala
+++ scala-functional-design-patterns/src/main/scala/example/Hello.scala
@@ -1,5 +1,7 @@
 package example
 
+import example.cakepattern._
+
 object Hello extends App {
 
   def demoMemoization() {
@@ -9,5 +11,19 @@ object Hello extends App {
     System.out.println(s"MD5 for 'hello' is '${hasher.memoMd5("hello")}'.")
     System.out.println(s"MD5 for 'bye1' is '${hasher.memoMd5("bye1")}'.")
     System.out.println(s"MD5 for 'bye' is '${hasher.memoMd5("bye")}'.")
+  }
+
+  def testCakePattern() {
+ 
+    // Mix and match various implementations
+    
+    val mysql = new PersonService with MySQLDao with Log4j
+    mysql.createPerson(Person("Nilanjan"))
+
+    val postgres = new PersonService with PostGresDao with LogBack
+    postgres.createPerson(Person("Nilanjan"))
+    
+    // Compile time error : Self type check done here
+    // val postgres1 = new PersonService with PostGresDao 
   }
 }
